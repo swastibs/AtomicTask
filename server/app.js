2d7bba6367f7b90192c715120fc0aff392b77e63
swastisunder <swastisunderbadatya@team.bigscal.com>
2026-08-04 10:54:52 +0530

    server setup

--- server/app.js
+++ server/app.js
@@ -1,20 +1,13 @@
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+const app = express();
 
-let app = express();
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
