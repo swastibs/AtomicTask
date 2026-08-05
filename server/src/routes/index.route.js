ca485b2e67bc2049de8bb8759d40925254190ef1
swastisunder <swastisunderbadatya@team.bigscal.com>
2026-08-05 11:37:22 +0530

    refactor: remove server codebase and related files

--- server/src/routes/index.route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const healthRouter = require("./health.route");
-const authRouter = require("./auth.route");
-
-router.use("/health", healthRouter);
-router.use("/auth", authRouter);
-
-module.exports = router;
